a609b5c51966a9ac3e4da4e77cbf24282edf786c
Akshat Tomar <akshattomar@1909gmail.com>
2026-01-20 22:14:23 +0530

    Js session

--- script(day-02).js
+++ script(day-02).js
@@ -1,28 +1,57 @@
+//  let fruits = ["apple", "banana"];
+//  let newLength = fruits.push("cherry");
+//  console.log(new)
+
+//  let fruits = ["apple", "banana", "cherry"];
+//  let removed = fruits.pop();
+//  console.log(removed) //
+//  console.log(fruits)
+
 // Q1.
 
-// let marks=[32,11,7,24,19,35,10,0,15,17];
-
-// let res = marks.filter((x)=>{
-//     if(x>=16){
-//         console.log(x,"pass")
-//     }
-//     else{
-//         console.log(x,"fail")
-//     }
-// });
-
-// Q2. find the sum of all the elements inside an array with for loop.
-
-// let arr=[1,2,3,4,5]
-// let sum=0;
-// for(let i=0;i<arr.length;i++){
-//     sum=sum+arr[i];
+// let arr = [1,2,3,4,5]
+// let rev=[]
+// while(arr.length>0){
+//     rev.push(arr.pop())
 // }
-// console.log(sum)
+// console.log(rev)
 
-// using reduce method
 
-// let arr=[1,2,3,4,5]
+// Q2. 
 
-// let res = arr.reduce((acc,current)=>acc+current)
-// console.log(res);
+// let arr=[-3,7,-6,-11,13,8];
+// let res = []
+
+// while(arr.length>0){
+//   let val = arr.shift();
+//   if(val>0){
+//     res.push(val);
+//   }
+// }
+// console.log(res)
+
+
+// Q3. 
+
+// function palindrome(){
+// let arr=[1,2,3,2,1];
+// let original=[] //[1,2,3,2,1]
+// let reverse=[] //[1,2,3,2,1]
+
+// for(let i=0;i<arr.length;i++){
+//     original.push(arr[i]);
+// }
+
+// while(arr.length>0){
+//    reverse.push(arr.pop())
+// }
+
+// for(i=0;i<original.length;i++){
+//     if(original[i] !== reverse[i]){
+//         return "not a palindrome"
+//     }
+// }
+// return "palindrome"
+// }
+
+// console.log(palindrome())
