8ce63eb6b80ec811e7446a2db355c100e959c2dd
Akshat Tomar <akshattomar@1909gmail.com>
2026-01-22 11:51:07 +0530

    Js session

---
+++ script(day-04).js
@@ -0,0 +1,12 @@
+let user={
+    fullName: "Akshat",
+    address: "Gurugram",
+    mobileNo: 1234567890,
+    favColor: ["black","red","green"]
+}
+console.log(user["fullName"])
+console.log(user.fullName,user.address,user.mobileNo,user.favColor) // targeting indiviual value
+
+
+let date= new Date()
+console.log(date)
